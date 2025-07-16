f168eae1621b9c81836f7a4401418b8f4e40ae74
Sulfurbottom <397love@gmail.com>
2025-07-17 08:36:04 +0900

    App 컴포넌트에 로고, 아이콘 및 검색 기능을 추가하고, 레이아웃을 개선했습니다. MUI를 활용하여 UI 요소를 정리하고, 사용자 인터페이스를 향상시켰습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import Box from "@mui/material/Box";
 import BasicCustomChip from "./components/chip/BasicCustomChip";
 import Typography from "@mui/material/Typography";
+import logo from "./assets/logo.svg";
+import DockIcon from "./assets/dock_icon.svg";
+import ExpandCircleDownIcon from "@mui/icons-material/ExpandCircleDown";
+import { Divider, IconButton, InputAdornment, TextField } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import InfoOutlineIcon from "@mui/icons-material/InfoOutline";
+import SearchIcon from "@mui/icons-material/Search";
+import { grey } from "@mui/material/colors";
 
 function App() {
   return (
@@ -18,19 +26,86 @@ function App() {
           display: "flex",
           flexDirection: "row",
           alignItems: "center",
+          justifyContent: "space-between",
           padding: "4px 8px 4px 4px",
-          gap: "4px",
           position: "relative",
           width: "100%",
           height: "44px",
           background: "#000000",
         }}
       >
-        <Typography color="white">OPUS Terminal</Typography>
-        <BasicCustomChip label="ASIA" />
-        <Typography variant="tableHeader" color="white">
-          Vessel Name
-        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
+          <img src={logo} alt="logo" />
+          <Divider
+            orientation="vertical"
+            sx={{
+              height: "24px",
+            }}
+          />
+          <img src={DockIcon} alt="dock" />
+          <BasicCustomChip label="ASIA" />
+          <Typography variant="tableHeader" color="white">
+            Vessel Name
+          </Typography>
+          <IconButton>
+            <ExpandCircleDownIcon
+              sx={{
+                rotate: "90deg",
+              }}
+            />
+          </IconButton>
+          <Typography variant="tableHeader" color="white">
+            Voyage Info
+          </Typography>
+          <IconButton>
+            <ExpandCircleDownIcon sx={{ rotate: "270deg" }} />
+          </IconButton>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
+          <TextField
+            placeholder="Search"
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon sx={{ color: grey[300], fontSize: "16px" }} />
+                  </InputAdornment>
+                ),
+              },
+            }}
+            sx={{
+              "& .MuiInputBase-root": {
+                height: "26px",
+                width: "200px",
+                borderRadius: "4px",
+                backgroundColor: grey[800],
+                color: grey[300],
+                fontSize: "13px",
+                fontWeight: 400,
+              },
+            }}
+          />
+          <IconButton>
+            <InfoOutlineIcon sx={{ color: grey[300] }} />
+          </IconButton>
+          <IconButton>
+            <AccountCircleIcon sx={{ color: grey[300] }} />
+          </IconButton>
+        </Box>
       </Box>
       <Box
         component="section"
