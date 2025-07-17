70e781064e747280d9515e91a7883faed8db9b34
Sulfurbottom <397love@gmail.com>
2025-07-17 11:56:25 +0900

    타입그래피 변형 및 옵션 인터페이스를 정리하고, MUI 테마의 색상 및 타이포그래피 설정을 개선했습니다. TaskManager 컴포넌트에 하단 버튼 영역을 추가하여 UI를 확장하고, 버튼 스타일을 조정하여 사용자 경험을 향상시켰습니다.

--- src/components/pages/TaskManager.tsx
+++ src/components/pages/TaskManager.tsx
@@ -2,27 +2,76 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { customColors } from "../../theme";
 import { formatNumber } from "../../util";
+import Divider from "@mui/material/Divider";
+import Button from "@mui/material/Button";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 
 export default function TaskManager() {
   return (
     <Box component="div" display="flex" flexDirection="column" sx={{ flex: 1 }}>
       <Box flex={1}>11</Box>
-      <Box
-        height={48}
-        bgcolor="white"
-        borderTop={1}
-        borderColor={customColors.components.rating.enabledBorder}
-        display="flex"
-        justifyContent="flex-end"
-        alignItems="center"
-        paddingRight={2}
-        gap={1.5}
-      >
-        <Typography variant="buttonLarge">TOTAL</Typography>
-        <Typography variant="buttonLarge" color="info">
-          {formatNumber(22100)}
-        </Typography>
+      <BottomButtonArea />
+    </Box>
+  );
+}
+
+function BottomButtonArea() {
+  return (
+    <Box
+      height={48}
+      bgcolor="white"
+      borderTop={1}
+      borderColor={customColors.components.rating.enabledBorder}
+      display="flex"
+      justifyContent="flex-end"
+      alignItems="center"
+      paddingRight={2}
+      gap={1.5}
+    >
+      <Typography variant="buttonLarge">TOTAL</Typography>
+      <Typography variant="buttonLarge" color="info">
+        {formatNumber(22100)}
+      </Typography>
+      <Divider orientation="vertical" sx={{ height: "24px" }} />
+      <Typography variant="buttonLarge">START</Typography>
+      <Box display="flex" gap={1}>
+        <Button variant="outlined" color="info" disabled>
+          RS
+        </Button>
+        <Button variant="outlined" color="info" disabled>
+          RS01
+        </Button>
+        <Button variant="outlined" color="info" disabled>
+          TP3-01-A-3
+        </Button>
+        <Button variant="outlined" color="info" disabled>
+          2024-07-30 16:15
+        </Button>
       </Box>
+      <KeyboardDoubleArrowRightIcon color="action" />
+      <Typography variant="buttonLarge">END</Typography>
+      <Box display="flex" gap={1}>
+        <Button variant="outlined" color="info">
+          RS
+        </Button>
+        <Button variant="outlined" color="info">
+          RS01
+        </Button>
+        <Button variant="outlined" color="info">
+          TP3-01-A-3
+        </Button>
+        <Button variant="contained" color="info">
+          RE-ALLOCATION
+        </Button>
+        <Button variant="contained" color="info">
+          COMPLETE
+        </Button>
+      </Box>
+
+      <Divider orientation="vertical" sx={{ height: "24px" }} />
+      <Button variant="contained" color="info">
+        ROLL BACK
+      </Button>
     </Box>
   );
 }
