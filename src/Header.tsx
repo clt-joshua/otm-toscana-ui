7edc71da1e7d2fb031a72d7c0e678b22deffb31e
Sulfurbottom <397love@gmail.com>
2025-07-17 09:20:16 +0900

    코드 포맷을 일관되게 수정하고, App 및 Header 컴포넌트의 구조를 정리했습니다. CheckableChip 컴포넌트의 아이콘 표시 방식을 개선하여 체크 상태를 시각적으로 명확하게 하였습니다.

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,12 +3,12 @@ import ExpandCircleDownIcon from "@mui/icons-material/ExpandCircleDown";
 import InfoOutlineIcon from "@mui/icons-material/InfoOutline";
 import SearchIcon from "@mui/icons-material/Search";
 import {
-	Box,
-	Divider,
-	IconButton,
-	InputAdornment,
-	TextField,
-	Typography,
+  Box,
+  Divider,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
 } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import { useState } from "react";
@@ -18,101 +18,101 @@ import BasicCustomChip from "./components/chip/BasicCustomChip";
 import CheckableChip from "./components/chip/CheckableChip";
 
 export default function Header() {
-	const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(false);
 
-	return (
-		<Box
-			component="header"
-			sx={{
-				display: "flex",
-				flexDirection: "row",
-				alignItems: "center",
-				justifyContent: "space-between",
-				padding: "4px 8px 4px 4px",
-				position: "relative",
-				width: "100%",
-				height: "44px",
-				background: "#000000",
-			}}
-		>
-			<Box
-				sx={{
-					display: "flex",
-					flexDirection: "row",
-					alignItems: "center",
-					gap: "8px",
-				}}
-			>
-				<img src={logo} alt="logo" />
-				<Divider
-					orientation="vertical"
-					sx={{
-						height: "24px",
-					}}
-				/>
-				<img src={DockIcon} alt="dock" />
-				<BasicCustomChip label="ASIA" />
-				<Typography variant="tableHeader" color="white">
-					Vessel Name
-				</Typography>
-				<IconButton>
-					<ExpandCircleDownIcon
-						sx={{
-							rotate: "90deg",
-						}}
-					/>
-				</IconButton>
-				<Typography variant="tableHeader" color="white">
-					Voyage Info
-				</Typography>
-				<IconButton>
-					<ExpandCircleDownIcon sx={{ rotate: "270deg" }} />
-				</IconButton>
-			</Box>
-			<Box
-				sx={{
-					display: "flex",
-					flexDirection: "row",
-					alignItems: "center",
-					gap: "8px",
-				}}
-			>
-				<TextField
-					placeholder="Search"
-					slotProps={{
-						input: {
-							startAdornment: (
-								<InputAdornment position="start">
-									<SearchIcon sx={{ color: grey[300], fontSize: "16px" }} />
-								</InputAdornment>
-							),
-						},
-					}}
-					sx={{
-						"& .MuiInputBase-root": {
-							height: "26px",
-							width: "200px",
-							borderRadius: "4px",
-							backgroundColor: grey[800],
-							color: grey[300],
-							fontSize: "13px",
-							fontWeight: 400,
-						},
-					}}
-				/>
-				<CheckableChip
-					label="CheckableChip"
-					color="info"
-					checked={checked}
-					onClick={() => setChecked(!checked)}
-				/>
-				<IconButton>
-					<InfoOutlineIcon sx={{ color: grey[300] }} />
-				</IconButton>
-				<IconButton>
-					<AccountCircleIcon sx={{ color: grey[300] }} />
-				</IconButton>
-			</Box>
-		</Box>
-	);
+  return (
+    <Box
+      component="header"
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "4px 8px 4px 4px",
+        position: "relative",
+        width: "100%",
+        height: "44px",
+        background: "#000000",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: "8px",
+        }}
+      >
+        <img src={logo} alt="logo" />
+        <Divider
+          orientation="vertical"
+          sx={{
+            height: "24px",
+          }}
+        />
+        <img src={DockIcon} alt="dock" />
+        <BasicCustomChip label="ASIA" />
+        <Typography variant="tableHeader" color="white">
+          Vessel Name
+        </Typography>
+        <IconButton>
+          <ExpandCircleDownIcon
+            sx={{
+              rotate: "90deg",
+            }}
+          />
+        </IconButton>
+        <Typography variant="tableHeader" color="white">
+          Voyage Info
+        </Typography>
+        <IconButton>
+          <ExpandCircleDownIcon sx={{ rotate: "270deg" }} />
+        </IconButton>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: "8px",
+        }}
+      >
+        <TextField
+          placeholder="Search"
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon sx={{ color: grey[300], fontSize: "16px" }} />
+                </InputAdornment>
+              ),
+            },
+          }}
+          sx={{
+            "& .MuiInputBase-root": {
+              height: "26px",
+              width: "200px",
+              borderRadius: "4px",
+              backgroundColor: grey[800],
+              color: grey[300],
+              fontSize: "13px",
+              fontWeight: 400,
+            },
+          }}
+        />
+        <CheckableChip
+          label="CheckableChip"
+          color="info"
+          checked={checked}
+          onClick={() => setChecked(!checked)}
+        />
+        <IconButton>
+          <InfoOutlineIcon sx={{ color: grey[300] }} />
+        </IconButton>
+        <IconButton>
+          <AccountCircleIcon sx={{ color: grey[300] }} />
+        </IconButton>
+      </Box>
+    </Box>
+  );
 }
--- src/components/chip/CheckableChip.tsx
+++ src/components/chip/CheckableChip.tsx
@@ -4,36 +4,36 @@ import Avatar from "@mui/material/Avatar";
 import Chip, { type ChipProps } from "@mui/material/Chip";
 
 export default function CheckableChip(props: ChipProps & { checked: boolean }) {
-	const { checked, label, ...rest } = props;
-	return (
-		<Chip
-			icon={
-				checked ? (
-					<Avatar
-						sx={{ backgroundColor: "info.dark", width: "18px", height: "18px" }}
-					>
-						<CheckIcon sx={{ fontSize: "18px", color: "white" }} />
-					</Avatar>
-				) : undefined
-			}
-			sx={{
-				"& .MuiChip-label": {
-					paddingRight: checked ? "4px" : undefined,
-					display: "flex",
-					alignItems: "center",
-				},
-			}}
-			label={
-				<>
-					{label}
-					{checked && (
-						<ArrowDropDownIcon sx={{ fontSize: "18px", ml: "4px" }} />
-					)}
-				</>
-			}
-			color={checked ? "info" : "default"}
-			variant={checked ? "filled" : "outlined"}
-			{...rest}
-		/>
-	);
+  const { checked, label, ...rest } = props;
+  return (
+    <Chip
+      icon={
+        checked ? (
+          <Avatar
+            sx={{ backgroundColor: "info.dark", width: "18px", height: "18px" }}
+          >
+            <CheckIcon sx={{ fontSize: "18px", color: "white" }} />
+          </Avatar>
+        ) : undefined
+      }
+      sx={{
+        "& .MuiChip-label": {
+          paddingRight: checked ? "4px" : undefined,
+          display: "flex",
+          alignItems: "center",
+        },
+      }}
+      label={
+        <>
+          {label}
+          {checked && (
+            <ArrowDropDownIcon sx={{ fontSize: "18px", ml: "4px" }} />
+          )}
+        </>
+      }
+      color={checked ? "info" : "default"}
+      variant={checked ? "filled" : "outlined"}
+      {...rest}
+    />
+  );
 }
