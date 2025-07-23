4c33785b360df70781ffb3da1de5d453231fd3f4
Sulfurbottom <397love@gmail.com>
2025-07-23 10:20:24 +0900

    사이드 내비게이션에 새로운 메뉴 아이템과 아이콘을 추가하여 사용자 인터페이스를 확장했습니다. 또한, 테마 색상을 업데이트하여 시각적 일관성을 개선했습니다.

--- src/SideNavigation.tsx
+++ src/SideNavigation.tsx
@@ -1,6 +1,20 @@
 import Box from "@mui/material/Box";
 import IconMenuMaster from "./assets/icon_menu_master.svg?react";
 import IconMenuDocument from "./assets/icon_menu_document.svg?react";
+import IconMenuInventory from "./assets/icon_menu_inventory.svg?react";
+import IconMenuTruck from "./assets/icon_menu_truck.svg?react";
+import IconMenuVessel from "./assets/icon_menu_ship.svg?react";
+import IconMenuYard from "./assets/icon_menu_yard.svg?react";
+import IconMenuCfs from "./assets/icon_menu_warehouse.svg?react";
+import IconMenuOperation from "./assets/icon_menu_operation.svg?react";
+import IconMenuEquipment from "./assets/icon_menu_equipment.svg?react";
+import IconMenuBilling from "./assets/icon_menu_billing.svg?react";
+import IconMenuHr from "./assets/icon_menu_hr.svg?react";
+import IconMenuMonitoring from "./assets/icon_menu_monitoring.svg?react";
+import IconMenuDashboard from "./assets/icon_menu_dashboard.svg?react";
+import IconMenuInterface from "./assets/icon_menu_interface.svg?react";
+import IconMenuAdmin from "./assets/icon_menu_admin.svg?react";
+import theme from "./theme";
 
 const menuItems = [
   {
@@ -12,6 +26,71 @@ const menuItems = [
     id: "document",
     label: "Document",
     icon: <IconMenuDocument />,
+  },
+  {
+    id: "inventory",
+    label: "Inventory",
+    icon: <IconMenuInventory />,
+  },
+  {
+    id: "truck",
+    label: "Truck",
+    icon: <IconMenuTruck />,
+  },
+  {
+    id: "vessel",
+    label: "Vessel",
+    icon: <IconMenuVessel />,
+  },
+  {
+    id: "yard",
+    label: "Yard",
+    icon: <IconMenuYard />,
+  },
+  {
+    id: "cfs",
+    label: "CFS & Warehouse",
+    icon: <IconMenuCfs />,
+  },
+  {
+    id: "operation",
+    label: "Operation",
+    icon: <IconMenuOperation />,
+  },
+  {
+    id: "equipment",
+    label: "Equipment",
+    icon: <IconMenuEquipment />,
+  },
+  {
+    id: "billing",
+    label: "Billing",
+    icon: <IconMenuBilling />,
+  },
+  {
+    id: "hr",
+    label: "HR",
+    icon: <IconMenuHr />,
+  },
+  {
+    id: "monitoring",
+    label: "Monitoring",
+    icon: <IconMenuMonitoring />,
+  },
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    icon: <IconMenuDashboard />,
+  },
+  {
+    id: "interface",
+    label: "Interface",
+    icon: <IconMenuInterface />,
+  },
+  {
+    id: "admin",
+    label: "Admin",
+    icon: <IconMenuAdmin />,
   },
 ];
 
@@ -40,6 +119,7 @@ export default function SideNavigation() {
             index === menuItems.length - 1 ? "none" : "1px solid #E0E0E0"
           }
           sx={{
+            color: theme.palette.action.active,
             cursor: "pointer",
             "&:hover": {
               backgroundColor: "rgba(0, 0, 0, 0.04)",
