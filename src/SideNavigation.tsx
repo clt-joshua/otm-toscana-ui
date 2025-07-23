e7a6a87c8bc3efaf2614365334bb43966c07e36a
Sulfurbottom <397love@gmail.com>
2025-07-23 10:22:43 +0900

    코드 포맷을 일관되게 수정하고, App.tsx, SideNavigation.tsx, BasicContainer.tsx, DetailContainer.tsx, BasicDataGrid.tsx 등 여러 컴포넌트의 레이아웃을 개선하여 사용자 인터페이스를 향상시켰습니다. 또한, VoyageInformation.tsx와 VoyageSchedule.tsx에서 새로운 입력 필드와 버튼을 추가하여 기능성을 강화했습니다.

--- src/SideNavigation.tsx
+++ src/SideNavigation.tsx
@@ -1,134 +1,134 @@
 import Box from "@mui/material/Box";
-import IconMenuMaster from "./assets/icon_menu_master.svg?react";
-import IconMenuDocument from "./assets/icon_menu_document.svg?react";
-import IconMenuInventory from "./assets/icon_menu_inventory.svg?react";
-import IconMenuTruck from "./assets/icon_menu_truck.svg?react";
-import IconMenuVessel from "./assets/icon_menu_ship.svg?react";
-import IconMenuYard from "./assets/icon_menu_yard.svg?react";
-import IconMenuCfs from "./assets/icon_menu_warehouse.svg?react";
-import IconMenuOperation from "./assets/icon_menu_operation.svg?react";
-import IconMenuEquipment from "./assets/icon_menu_equipment.svg?react";
-import IconMenuBilling from "./assets/icon_menu_billing.svg?react";
-import IconMenuHr from "./assets/icon_menu_hr.svg?react";
-import IconMenuMonitoring from "./assets/icon_menu_monitoring.svg?react";
-import IconMenuDashboard from "./assets/icon_menu_dashboard.svg?react";
-import IconMenuInterface from "./assets/icon_menu_interface.svg?react";
 import IconMenuAdmin from "./assets/icon_menu_admin.svg?react";
+import IconMenuBilling from "./assets/icon_menu_billing.svg?react";
+import IconMenuDashboard from "./assets/icon_menu_dashboard.svg?react";
+import IconMenuDocument from "./assets/icon_menu_document.svg?react";
+import IconMenuEquipment from "./assets/icon_menu_equipment.svg?react";
+import IconMenuHr from "./assets/icon_menu_hr.svg?react";
+import IconMenuInterface from "./assets/icon_menu_interface.svg?react";
+import IconMenuInventory from "./assets/icon_menu_inventory.svg?react";
+import IconMenuMaster from "./assets/icon_menu_master.svg?react";
+import IconMenuMonitoring from "./assets/icon_menu_monitoring.svg?react";
+import IconMenuOperation from "./assets/icon_menu_operation.svg?react";
+import IconMenuVessel from "./assets/icon_menu_ship.svg?react";
+import IconMenuTruck from "./assets/icon_menu_truck.svg?react";
+import IconMenuCfs from "./assets/icon_menu_warehouse.svg?react";
+import IconMenuYard from "./assets/icon_menu_yard.svg?react";
 import theme from "./theme";
 
 const menuItems = [
-  {
-    id: "master",
-    label: "Master",
-    icon: <IconMenuMaster />,
-  },
-  {
-    id: "document",
-    label: "Document",
-    icon: <IconMenuDocument />,
-  },
-  {
-    id: "inventory",
-    label: "Inventory",
-    icon: <IconMenuInventory />,
-  },
-  {
-    id: "truck",
-    label: "Truck",
-    icon: <IconMenuTruck />,
-  },
-  {
-    id: "vessel",
-    label: "Vessel",
-    icon: <IconMenuVessel />,
-  },
-  {
-    id: "yard",
-    label: "Yard",
-    icon: <IconMenuYard />,
-  },
-  {
-    id: "cfs",
-    label: "CFS & Warehouse",
-    icon: <IconMenuCfs />,
-  },
-  {
-    id: "operation",
-    label: "Operation",
-    icon: <IconMenuOperation />,
-  },
-  {
-    id: "equipment",
-    label: "Equipment",
-    icon: <IconMenuEquipment />,
-  },
-  {
-    id: "billing",
-    label: "Billing",
-    icon: <IconMenuBilling />,
-  },
-  {
-    id: "hr",
-    label: "HR",
-    icon: <IconMenuHr />,
-  },
-  {
-    id: "monitoring",
-    label: "Monitoring",
-    icon: <IconMenuMonitoring />,
-  },
-  {
-    id: "dashboard",
-    label: "Dashboard",
-    icon: <IconMenuDashboard />,
-  },
-  {
-    id: "interface",
-    label: "Interface",
-    icon: <IconMenuInterface />,
-  },
-  {
-    id: "admin",
-    label: "Admin",
-    icon: <IconMenuAdmin />,
-  },
+	{
+		id: "master",
+		label: "Master",
+		icon: <IconMenuMaster />,
+	},
+	{
+		id: "document",
+		label: "Document",
+		icon: <IconMenuDocument />,
+	},
+	{
+		id: "inventory",
+		label: "Inventory",
+		icon: <IconMenuInventory />,
+	},
+	{
+		id: "truck",
+		label: "Truck",
+		icon: <IconMenuTruck />,
+	},
+	{
+		id: "vessel",
+		label: "Vessel",
+		icon: <IconMenuVessel />,
+	},
+	{
+		id: "yard",
+		label: "Yard",
+		icon: <IconMenuYard />,
+	},
+	{
+		id: "cfs",
+		label: "CFS & Warehouse",
+		icon: <IconMenuCfs />,
+	},
+	{
+		id: "operation",
+		label: "Operation",
+		icon: <IconMenuOperation />,
+	},
+	{
+		id: "equipment",
+		label: "Equipment",
+		icon: <IconMenuEquipment />,
+	},
+	{
+		id: "billing",
+		label: "Billing",
+		icon: <IconMenuBilling />,
+	},
+	{
+		id: "hr",
+		label: "HR",
+		icon: <IconMenuHr />,
+	},
+	{
+		id: "monitoring",
+		label: "Monitoring",
+		icon: <IconMenuMonitoring />,
+	},
+	{
+		id: "dashboard",
+		label: "Dashboard",
+		icon: <IconMenuDashboard />,
+	},
+	{
+		id: "interface",
+		label: "Interface",
+		icon: <IconMenuInterface />,
+	},
+	{
+		id: "admin",
+		label: "Admin",
+		icon: <IconMenuAdmin />,
+	},
 ];
 
 export default function SideNavigation() {
-  return (
-    <Box
-      component="aside"
-      sx={{
-        minWidth: "42px",
-        width: "42px",
-        boxShadow:
-          "0px 3px 3px -2px rgba(0, 0, 0, 0.2), 0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12)",
-        zIndex: 2,
-      }}
-    >
-      {menuItems.map((item, index) => (
-        <Box
-          key={item.id}
-          padding={1}
-          width="42px"
-          height="42px"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          borderBottom={
-            index === menuItems.length - 1 ? "none" : "1px solid #E0E0E0"
-          }
-          sx={{
-            color: theme.palette.action.active,
-            cursor: "pointer",
-            "&:hover": {
-              backgroundColor: "rgba(0, 0, 0, 0.04)",
-            },
-          }}
-        >
-          {item.icon}
-        </Box>
-      ))}
-    </Box>
-  );
+	return (
+		<Box
+			component="aside"
+			sx={{
+				minWidth: "42px",
+				width: "42px",
+				boxShadow:
+					"0px 3px 3px -2px rgba(0, 0, 0, 0.2), 0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12)",
+				zIndex: 2,
+			}}
+		>
+			{menuItems.map((item, index) => (
+				<Box
+					key={item.id}
+					padding={1}
+					width="42px"
+					height="42px"
+					display="flex"
+					justifyContent="center"
+					alignItems="center"
+					borderBottom={
+						index === menuItems.length - 1 ? "none" : "1px solid #E0E0E0"
+					}
+					sx={{
+						color: theme.palette.action.active,
+						cursor: "pointer",
+						"&:hover": {
+							backgroundColor: "rgba(0, 0, 0, 0.04)",
+						},
+					}}
+				>
+					{item.icon}
+				</Box>
+			))}
+		</Box>
+	);
 }
--- src/constants/taskManagerData.ts
+++ src/constants/taskManagerData.ts
@@ -1,161 +1,161 @@
 import { faker } from "@faker-js/faker";
 import React from "react";
 import ShipIcon from "../assets/icon_menu_ship.svg?react";
-import TerminalIcon from "../assets/icon_terminal.svg?react";
 import TruckIcon from "../assets/icon_menu_truck.svg?react";
+import TerminalIcon from "../assets/icon_terminal.svg?react";
 import type {
-  ButtonData,
-  ChipGroup,
-  TaskManagerTableProps,
+	ButtonData,
+	ChipGroup,
+	TaskManagerTableProps,
 } from "../pages/taskManager/TaskManagerTypes";
 
 export const CHIP_GROUPS: ChipGroup[] = [
-  {
-    id: "ship",
-    items: [
-      {
-        label: "Ship",
-        type: "icon",
-        icon: React.createElement(ShipIcon),
-        color: "white",
-        bgColor: "rgba(21, 179, 205, 1)",
-      },
-      {
-        label: "DS",
-        type: "text",
-        color: "white",
-        bgColor: "rgba(14, 142, 194, 1)",
-      },
-      {
-        label: "LD",
-        type: "text",
-        color: "white",
-        bgColor: "rgba(240, 144, 40, 1)",
-      },
-      {
-        label: "RS",
-        type: "text",
-        color: "white",
-        bgColor: "rgba(58, 58, 58, 1)",
-      },
-      {
-        label: "LC",
-        type: "text",
-        color: "black",
-        bgColor: "rgba(246, 213, 109, 1)",
-      },
-      {
-        label: "DE",
-        type: "text",
-        color: "black",
-        bgColor: "rgba(217, 214, 48, 1)",
-      },
-    ],
-  },
-  {
-    id: "truck",
-    items: [
-      {
-        label: "Truck",
-        type: "icon",
-        icon: React.createElement(TruckIcon),
-        bgColor: "rgba(68, 162, 77, 1)",
-        color: "white",
-      },
-      {
-        label: "GI",
-        type: "text",
-        color: "black",
-        bgColor: "rgba(72, 210, 98, 1)",
-      },
-      {
-        label: "GO",
-        type: "text",
-        color: "white",
-        bgColor: "rgba(250, 53, 51, 1)",
-      },
-      {
-        label: "GC",
-        type: "text",
-        color: "white",
-        bgColor: "rgba(160, 160, 160, 1)",
-      },
-    ],
-  },
-  {
-    id: "terminal",
-    items: [
-      {
-        label: "Terminal",
-        type: "icon",
-        icon: React.createElement(TerminalIcon),
-        color: "white",
-        bgColor: "rgba(251, 140, 0, 1)",
-      },
-      {
-        label: "HK",
-        type: "text",
-        color: "white",
-        bgColor: "rgba(91, 56, 170, 1)",
-      },
-      {
-        label: "AH",
-        type: "text",
-        color: "black",
-        bgColor: "rgba(206, 227, 238, 1)",
-      },
-      {
-        label: "CT",
-        type: "text",
-        color: "white",
-        bgColor: "rgba(22, 59, 84, 1)",
-      },
-    ],
-  },
+	{
+		id: "ship",
+		items: [
+			{
+				label: "Ship",
+				type: "icon",
+				icon: React.createElement(ShipIcon),
+				color: "white",
+				bgColor: "rgba(21, 179, 205, 1)",
+			},
+			{
+				label: "DS",
+				type: "text",
+				color: "white",
+				bgColor: "rgba(14, 142, 194, 1)",
+			},
+			{
+				label: "LD",
+				type: "text",
+				color: "white",
+				bgColor: "rgba(240, 144, 40, 1)",
+			},
+			{
+				label: "RS",
+				type: "text",
+				color: "white",
+				bgColor: "rgba(58, 58, 58, 1)",
+			},
+			{
+				label: "LC",
+				type: "text",
+				color: "black",
+				bgColor: "rgba(246, 213, 109, 1)",
+			},
+			{
+				label: "DE",
+				type: "text",
+				color: "black",
+				bgColor: "rgba(217, 214, 48, 1)",
+			},
+		],
+	},
+	{
+		id: "truck",
+		items: [
+			{
+				label: "Truck",
+				type: "icon",
+				icon: React.createElement(TruckIcon),
+				bgColor: "rgba(68, 162, 77, 1)",
+				color: "white",
+			},
+			{
+				label: "GI",
+				type: "text",
+				color: "black",
+				bgColor: "rgba(72, 210, 98, 1)",
+			},
+			{
+				label: "GO",
+				type: "text",
+				color: "white",
+				bgColor: "rgba(250, 53, 51, 1)",
+			},
+			{
+				label: "GC",
+				type: "text",
+				color: "white",
+				bgColor: "rgba(160, 160, 160, 1)",
+			},
+		],
+	},
+	{
+		id: "terminal",
+		items: [
+			{
+				label: "Terminal",
+				type: "icon",
+				icon: React.createElement(TerminalIcon),
+				color: "white",
+				bgColor: "rgba(251, 140, 0, 1)",
+			},
+			{
+				label: "HK",
+				type: "text",
+				color: "white",
+				bgColor: "rgba(91, 56, 170, 1)",
+			},
+			{
+				label: "AH",
+				type: "text",
+				color: "black",
+				bgColor: "rgba(206, 227, 238, 1)",
+			},
+			{
+				label: "CT",
+				type: "text",
+				color: "white",
+				bgColor: "rgba(22, 59, 84, 1)",
+			},
+		],
+	},
 ];
 
 export const STATUS_OPTIONS = ["Initialize", "Active", "Complete", "Suspended"];
 
 export const TASK_MANAGER_TABLE_DATA: TaskManagerTableProps[] = Array.from(
-  { length: 100 },
-  (_, idx) => ({
-    status: faker.helpers.arrayElement([
-      "Initialize",
-      "Active",
-      "Complete",
-      "Suspended",
-    ]),
-    cargo: faker.string.alpha(4).toUpperCase() + faker.string.numeric(6),
-    planType: faker.helpers.arrayElement(["AH", "GO", "GI", "GC", "HK", "CT"]),
-    type: faker.helpers.arrayElement(["MSK", "NYK"]),
-    opr: faker.helpers.arrayElement(["MYPEN", "MYPKG"]),
-    pod: "",
-    npod: faker.helpers.arrayElement(["", "NZTIU", "FRCSJ", "CNHUA"]),
-    fpod: "CNCJG",
-    from: "TP3-02-B-1",
-    to: "TP3-02-B-2",
-    transporter: "EH12345",
-    startEq: "RS01",
-    fa: "Center",
-    recDelCardNo: "",
-    waiting: "",
-    planSeq: (idx + 1).toString(),
-    opProcess: "G2Y",
-    port: "",
-  })
+	{ length: 100 },
+	(_, idx) => ({
+		status: faker.helpers.arrayElement([
+			"Initialize",
+			"Active",
+			"Complete",
+			"Suspended",
+		]),
+		cargo: faker.string.alpha(4).toUpperCase() + faker.string.numeric(6),
+		planType: faker.helpers.arrayElement(["AH", "GO", "GI", "GC", "HK", "CT"]),
+		type: faker.helpers.arrayElement(["MSK", "NYK"]),
+		opr: faker.helpers.arrayElement(["MYPEN", "MYPKG"]),
+		pod: "",
+		npod: faker.helpers.arrayElement(["", "NZTIU", "FRCSJ", "CNHUA"]),
+		fpod: "CNCJG",
+		from: "TP3-02-B-1",
+		to: "TP3-02-B-2",
+		transporter: "EH12345",
+		startEq: "RS01",
+		fa: "Center",
+		recDelCardNo: "",
+		waiting: "",
+		planSeq: (idx + 1).toString(),
+		opProcess: "G2Y",
+		port: "",
+	}),
 );
 
 export const START_BUTTON_DATA: ButtonData[] = [
-  { label: "RS", variant: "outlined", disabled: true },
-  { label: "RS01", variant: "outlined", disabled: true },
-  { label: "TP3-01-A-3", variant: "outlined", disabled: true },
-  { label: "2024-07-30 16:15", variant: "outlined", disabled: true },
+	{ label: "RS", variant: "outlined", disabled: true },
+	{ label: "RS01", variant: "outlined", disabled: true },
+	{ label: "TP3-01-A-3", variant: "outlined", disabled: true },
+	{ label: "2024-07-30 16:15", variant: "outlined", disabled: true },
 ];
 
 export const END_BUTTON_DATA: ButtonData[] = [
-  { label: "RS", variant: "outlined" },
-  { label: "RS01", variant: "outlined" },
-  { label: "TP3-01-A-3", variant: "outlined" },
-  { label: "RE-ALLOCATION", variant: "contained", action: "reallocation" },
-  { label: "COMPLETE", variant: "contained", action: "complete" },
+	{ label: "RS", variant: "outlined" },
+	{ label: "RS01", variant: "outlined" },
+	{ label: "TP3-01-A-3", variant: "outlined" },
+	{ label: "RE-ALLOCATION", variant: "contained", action: "reallocation" },
+	{ label: "COMPLETE", variant: "contained", action: "complete" },
 ];
